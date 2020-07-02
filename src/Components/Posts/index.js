98635bfcefd0ea26b29acc427931d9e871b74356
Daniela Aparecida Amadeu de Proença <amadev@amadev.localhost>
2020-07-02 16:37:27 -0300

    making the app works

--- src/Components/Posts/index.js
+++ src/Components/Posts/index.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './styled.css'
 
 export default class Posts extends Component {
     state = {
-        post: [],
-        id: 1,
+        post: {},
+        id: 0
     }
 
     getPosts = () => {
-        const ide = this.state.id
+        const ide = this.state.id + 1
         axios.get(`https://jsonplaceholder.typicode.com/posts/${ide}`)
             .then(res => {
                 this.setState({
                     post: res.data,
+                    id: ide
                 })
             })
     }
 
-    componentDidMount = () => {
+    componentDidMount() {
         this.getPosts()
-    }
 
-    handleClick = () => {
-        this.setState({
-            id: this.state.id + 1
-        })
     }
 
     render() {
         return (
             <>
                 <h1>{this.state.post.title}</h1>
-                <button onClick={this.handleClick}>Next</button>
+                <p>{this.state.post.body}</p>
+                <button onClick={this.getPosts}>Next</button>
             </>
         )
     }
